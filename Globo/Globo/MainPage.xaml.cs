d37e2508841d9915d3cbaaf90681402093e2914a
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-04-30 17:47:41 -0300

    Adicionar arquivos de projeto.

---
+++ Globo/Globo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Globo.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Globo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private const string url = "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fg1.globo.com%2Fdynamo%2Frss2.xml";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+
+        protected void LoadData()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadStringCompleted += WebClient_DownloadStringCompleted;
+            webClient.DownloadStringAsync(new Uri(url));
+        }
+
+        public void WebClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            Notice data = JsonConvert.DeserializeObject<Notice>(e.Result);
+            NewsListView.ItemsSource = data.Items;
+        }
+    }
+}
---
+++ Globo/Globo/Model/Notice.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Globo.Model
+{
+    public class Feed
+    {
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+    }
+
+    public class Enclosure
+    {
+
+        [JsonProperty("link")]
+        public string Link { get; set; }
+    }
+
+    public class Item
+    {
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("pubDate")]
+        public string PubDate { get; set; }
+
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("thumbnail")]
+        public string Thumbnail { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("enclosure")]
+        public Enclosure Enclosure { get; set; }
+
+        [JsonProperty("categories")]
+        public IList<string> Categories { get; set; }
+    }
+
+    public class Notice
+    {
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("feed")]
+        public Feed Feed { get; set; }
+
+        [JsonProperty("items")]
+        public IList<Item> Items { get; set; }
+    }
+}
