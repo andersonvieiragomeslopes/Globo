c2489282d05306ead1d99d9a3131fea6f0ffe999
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-05-03 18:52:02 -0300

    ffimage added

--- Globo/Globo.Android/MainActivity.cs
+++ Globo/Globo.Android/MainActivity.cs
@@ -18,9 +18,12 @@ namespace Globo.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
